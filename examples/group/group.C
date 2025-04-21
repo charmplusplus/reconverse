d5c955a5c04c58df1fbda91bb8c3edbf69bf7071
Ritvik Rao <rsrao2@illinois.edu>
2025-04-21 08:56:36 -0500

    group example

---
+++ examples/group/group.C
@@ -0,0 +1,56 @@
+#include "converse.h"
+#include <stdio.h>
+#include <pthread.h>
+
+CpvDeclare(int, exitHandlerId);
+
+struct Message
+{
+  CmiMessageHeader header;
+};
+
+void stop_handler(void *vmsg)
+{
+  CsdExitScheduler();
+}
+
+void ping_handler(void *vmsg)
+{
+  printf("Group send received on pe %i\n", CmiMyPe());
+  stop_handler(NULL);
+}
+
+CmiStartFn mymain(int argc, char **argv)
+{
+  printf("My PE is %d\n", CmiMyRank());
+
+  int handlerId = CmiRegisterHandler(ping_handler);
+
+  if(CmiNumPes() < 3)
+  {
+    printf("This example requires at least 3 PEs\n");
+    CmiAbort("Not enough PEs");
+  }
+
+  if (CmiMyPe() == 0)
+  {
+    //make a group
+    const int group_size = 3;
+    int pes[group_size] = {0, 1, 2};
+    CmiGroup group = CmiEstablishGroup(group_size, pes);
+    
+    // create a message
+    Message *msg = new Message;
+    msg->header.messageSize = sizeof(Message);
+    msg->header.handlerId = handlerId;
+    CmiSyncMulticastAndFree(group, sizeof(Message), msg);
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv)
+{
+  ConverseInit(argc, argv, (CmiStartFn)mymain);
+  return 0;
+}
