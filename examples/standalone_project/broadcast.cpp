a60903fcb56eb46bc056a7026501cc3817f249fe
Jiakun Yan <jiakunyan1998@gmail.com>
2025-07-11 10:35:35 -0500

    add example standalone_project

---
+++ examples/standalone_project/broadcast.cpp
@@ -0,0 +1,38 @@
+#include "converse.h"
+#include <pthread.h>
+#include <stdio.h>
+
+CpvDeclare(int, exitHandlerId);
+
+struct Message {
+  CmiMessageHeader header;
+};
+
+void stop_handler(void *vmsg) { CsdExitScheduler(); }
+
+void ping_handler(void *vmsg) {
+  printf("PING HANDLER CALLED ON PE %i\n", CmiMyPe());
+  stop_handler(NULL);
+}
+
+CmiStartFn mymain(int argc, char **argv) {
+  printf("My PE is %d\n", CmiMyPe());
+
+  int handlerId = CmiRegisterHandler(ping_handler);
+
+  if (CmiMyPe() == 0) {
+    // create a message
+    Message *msg = new Message;
+    msg->header.handlerId = handlerId;
+    msg->header.messageSize = sizeof(Message);
+
+    CmiSyncBroadcastAllAndFree(msg->header.messageSize, msg);
+  }
+
+  return 0;
+}
+
+int main(int argc, char **argv) {
+  ConverseInit(argc, argv, (CmiStartFn)mymain);
+  return 0;
+}
