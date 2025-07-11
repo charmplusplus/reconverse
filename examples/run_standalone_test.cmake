9fcce34e486b202f36dbc4a35f08e43073f2bbf1
Jiakun Yan <jiakunyan1998@gmail.com>
2025-07-11 10:46:09 -0500

    add standalone_project as a cmake test

---
+++ examples/run_standalone_test.cmake
@@ -0,0 +1,30 @@
+# run_standalone_test.cmake
+
+set(STANDALONE_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/standalone_project")
+set(STANDALONE_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/standalone_build")
+
+file(MAKE_DIRECTORY "${STANDALONE_BINARY_DIR}")
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} -S "${STANDALONE_SOURCE_DIR}" -B "${STANDALONE_BINARY_DIR}"
+  RESULT_VARIABLE result
+)
+if(NOT result EQUAL 0)
+  message(FATAL_ERROR "CMake configuration failed for standalone_project")
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_COMMAND} --build "${STANDALONE_BINARY_DIR}"
+  RESULT_VARIABLE result
+)
+if(NOT result EQUAL 0)
+  message(FATAL_ERROR "Build failed for standalone_project")
+endif()
+
+execute_process(
+  COMMAND ${CMAKE_CTEST_COMMAND} --test-dir "${STANDALONE_BINARY_DIR}" --output-on-failure
+  RESULT_VARIABLE result
+)
+if(NOT result EQUAL 0)
+  message(FATAL_ERROR "Tests failed in standalone_project")
+endif()
