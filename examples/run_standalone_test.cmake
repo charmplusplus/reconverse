982bd1ece28c1008a7e1649fb1714f55e710c89b
Jiakun Yan <jiakunyan1998@gmail.com>
2025-07-12 15:42:26 -0500

    use local directory as the autofetch target to test the standalone example

--- examples/run_standalone_test.cmake
+++ examples/run_standalone_test.cmake
@@ -3,14 +3,21 @@
 # This script simulates building a reconverse application as a standalone project.
 # It will be run when `ctest` is invoked.
 
-set(STANDALONE_SOURCE_DIR "${PROJECT_CURRENT_SOURCE_DIR}/standalone_project")
+get_filename_component(SCRIPT_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+set(STANDALONE_SOURCE_DIR "${SCRIPT_DIR}/standalone_project")
 set(STANDALONE_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/standalone_build")
+
+# Use the real source path for reconverse (one level up from examples/)
+get_filename_component(RECONVERSE_LOCAL_SOURCE_DIR "${SCRIPT_DIR}/.." ABSOLUTE)
 
 file(MAKE_DIRECTORY "${STANDALONE_BINARY_DIR}")
 
 # Configure the standalone project
 execute_process(
-  COMMAND ${CMAKE_COMMAND} -S "${STANDALONE_SOURCE_DIR}" -B "${STANDALONE_BINARY_DIR}"
+  COMMAND ${CMAKE_COMMAND}
+          -S "${STANDALONE_SOURCE_DIR}"
+          -B "${STANDALONE_BINARY_DIR}"
+          -DFETCHCONTENT_SOURCE_RECONVERSE="${RECONVERSE_LOCAL_SOURCE_DIR}"  # for FetchContent
   RESULT_VARIABLE result
 )
 if(NOT result EQUAL 0)
