d1fcab7b05329440f04982f4a0b55644edc8b824
Sanjit Kumar <sanjitkmr112@gmail.com>
2026-07-16 17:45:35 -0400

    Increase pollProgress weight to 32

--- src/scheduler.cpp
+++ src/scheduler.cpp
@@ -116,7 +116,7 @@ void CmiQueueRegisterInitThread() {
   handlers.push_back(std::make_pair(pollConverseThreadQueue, 16));
   handlers.push_back(std::make_pair(pollNodePrioQueue, 1));
   handlers.push_back(std::make_pair(pollThreadPrioQueue, 16));
-  handlers.push_back(std::make_pair(pollProgress, 4));
+  handlers.push_back(std::make_pair(pollProgress, 32));
 #if CMK_TASKQUEUE
   handlers.push_back(std::make_pair(pollTaskQueue, 1));
 #endif
